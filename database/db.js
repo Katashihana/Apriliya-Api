fa8c06c375e4db66f12fe5cbc1d6f8141be189f7
Katashihana <89012832+Katashihana@users.noreply.github.com>
2022-02-28 15:48:23 +0700

    Add files via upload

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'isi dengan url database mongodb';
+var url = 'https://katashi-hana-api.herokuapp.com/';
 try {
-if(url == 'isi dengan url database mongodb') throw console.log(color('Configurasi database, var url belum diisi','red'));
+if(url == 'https://katashi-hana-api.herokuapp.com/') throw console.log(color('Configurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
